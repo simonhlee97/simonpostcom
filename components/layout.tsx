c787e81a9274f880465213a36a920362d08cb251
Simon Lee <simonhlee97@hotmail.com>
2021-12-23 16:42:34 +0900

    nextjs typescript blog simonpost

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,23 +1,67 @@
-import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
+import { BiHome } from 'react-icons/bi'
 
 type Props = {
-  preview?: boolean
-  children: React.ReactNode
+	preview?: boolean
+	children: React.ReactNode
 }
 
 const Layout = ({ preview, children }: Props) => {
-  return (
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
-  )
+	return (
+		<>
+			<Meta />
+			<div className="min-h-screen">
+				<Navbar />
+				<main>{children}</main>
+			</div>
+			<Footer />
+		</>
+	)
+}
+
+function Navbar() {
+	return (
+		<nav
+			className="
+          flex flex-wrap
+          items-center
+          justify-between
+          w-full
+          py-4
+          md:py-0
+          px-4
+          text-lg text-gray-700
+					bg-slate-100
+        ">
+			<div>
+				<a className="hover:text-blue-500" href="/">
+					<BiHome size={30} />
+				</a>
+			</div>
+
+			<div className="w-full md:flex md:items-center md:w-auto " id="menu">
+				<ul
+					className="
+              text-base text-gray-700
+              pt-4
+              md:flex
+              md:justify-between
+              md:pt-0">
+					<li>
+						<a className="md:p-4 py-2 block hover:text-blue-500" href="/about">
+							About
+						</a>
+					</li>
+					<li>
+						<a className="md:p-4 py-2 block hover:text-blue-500" href="/">
+							Blog
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	)
 }
 
 export default Layout
---
+++ pages/about.tsx
@@ -0,0 +1,53 @@
+import Container from '../components/container'
+import Layout from '../components/layout'
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa'
+
+export default function About() {
+	return (
+		<>
+			<Layout>
+				<Container>
+					<div className="py-2 m-auto max-w-prose">
+						<img
+							src="/assets/blog/authors/simonface.jpg"
+							className="mt-20 mb-4 mx-auto simonface"
+							alt="simonface"
+						/>
+						<p className="mb-8 text-3xl text-center">Hi, I'm Simon.</p>
+						<p className="mb-8">
+							I'm a Korean-born 'Hoosier' (a person from Indiana), so born in rice
+							fields, grew up in corn fields. I also lived in Chicago for about 10
+							years. Go Cubs.
+						</p>
+
+						<p className="mb-8">
+							I'm a web developer, books/podcast nerd, 4.0 tennis player, terrible
+							golfer, pizza/taco connoisseur, and (not ashamed to admit) a die-hard
+							Indiana Pacers fan.
+						</p>
+						<p className="mb-8">
+							My blog is mostly about tech and web development topics like JavaScript,
+							React, VueJS, HTML, CSS, databases, etc. and (occasionally) everyday
+							life as a Hoosier-expat in Korea.
+						</p>
+						<p className="mb-8 text-xl">Connect:</p>
+						<p className="mb-8 flex">
+							<FaGithubSquare
+								className="mr-4 hover:cursor-pointer hover:text-blue-600"
+								size={30}
+							/>
+							<FaLinkedin
+								className="mr-4 hover:cursor-pointer hover:text-blue-600"
+								size={30}
+							/>
+							<FaTwitterSquare
+								className="hover:cursor-pointer hover:text-blue-600"
+								size={30}
+							/>
+						</p>
+					</div>
+				</Container>
+			</Layout>
+		</>
+	)
+}
