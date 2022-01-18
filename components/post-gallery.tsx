4b39add71f74885a4efcd3d0f511298fe2ee0784
Simon Lee <simonhlee97@hotmail.com>
2022-01-18 12:06:54 +0900

    fix: deleted package-lock and removed a file not needed

--- components/post-gallery.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Gallery from 'react-photo-gallery'
-import { photos } from './photos'
-
-/* popout the browser and maximize to see more rows! -> */
-const PostGallery: React.FC = () => <Gallery photos={photos} />
-
-export default PostGallery
