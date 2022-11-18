53f68400f192a3a4e0f0b0a3fe50245bcc809950
Simon Lee <simonhlee97@hotmail.com>
2022-11-18 16:08:41 +0900

    remove unneeded files

--- pages/api/example.js
+++
@@ -1,9 +0,0 @@
-// const Innertube = require('youtubei.js');
-
-// async function handler(req, res) {
-//   const youtube = await new Innertube();
-//   const search = await youtube.search('espn first take');
-//   console.log(search);
-//   res.status(200).json()
-// }
-// export default handler
