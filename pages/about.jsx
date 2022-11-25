d4a95d19cfd1f069ad0b7cd5274067410d570c9c
Simon Lee <simonhlee97@hotmail.com>
2022-11-25 09:22:57 +0900

    fix url from localhost to api

--- pages/about.jsx
+++ pages/about.jsx
@@ -13,7 +13,7 @@ export default function About() {
 	// Form submit handler
 	const submitForm = async (e) => {
 		e.preventDefault()
-		const res = await fetch('http://localhost:3000/api/submit-form', {
+		const res = await fetch('/api/submit-form', {
 			method: 'POST',
 			body: JSON.stringify({ name, email, message }),
 		})
